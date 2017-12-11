6ea15daf60f8267582d1a0d0e10b667ba329ad8f
Jim Pick <jim@backblend.com>
2017-12-11 09:19:03 -0800

    Get multi-peer hyperdb writing working

--- usage1.js
+++ usage1.js
@@ -1,9 +1,23 @@
 var hyperdb = require('hyperdb')
 var hyperdiscovery = require('hyperdiscovery')
 
+var nodes = {
+  2: '9480f19b05299d50be41833699e7826b336cde39afcecab7664eaaf8fbf96994'
+}
+
 var db = hyperdb('./my.db', {valueEncoding: 'utf-8'})
 
 db.on('ready', function () {
+  console.log('Key', db.key.toString('hex'))
+  /*
+  db.authorize(nodes[2], function (err) {
+    if (err) {
+      console.error('Error', err)
+      return
+    }
+    console.log('Node 2 authorized')
+  })
+  */
   var sw = hyperdiscovery(db, {live: true})
   sw.on('connection', function (peer, type) {
     // console.log('got', peer, type)
@@ -14,12 +28,23 @@ db.on('ready', function () {
   })
 })
 
+/*
 db.put('/hello', 'world3', function (err) {
   if (err) throw err
+})
+*/
+
+function get() {
   db.get('/hello', function (err, nodes) {
     if (err) throw err
-    console.log('/hello --> ' + nodes[0].value)
-    console.log('db.local.discoveryKey', db.local.discoveryKey.toString('hex'))
-    console.log('db.local.key', db.local.key.toString('hex'))
+    if (!nodes) {
+      console.log('Node /hello not found')
+    } else {
+      console.log('/hello --> ' + nodes[0].value)
+    }
   })
-})
+}
+
+get()
+
+db.watch('/hello', get)
